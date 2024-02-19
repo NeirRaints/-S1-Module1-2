b8153aab82a82f305a66ea60e696472f0e00ca6a
Владилен Турышев <niggative99@mail.ru>
2024-02-19 15:03:05 +0500

    Добавил ограничения размера строк

--- ClinicBigBoarsWeb/Models/Patient.cs
+++ ClinicBigBoarsWeb/Models/Patient.cs
@@ -7,16 +7,22 @@ namespace ClinicBigBoarsWeb.Models
         [Key]
         public int PatientId { get; set; }
         public string? Photo {  get; set; }
+        [StringLength(50)]
         public required string Name { get; set; }
+        [StringLength(50)]
         public required string Surname { get; set; }
+        [StringLength(50)]
         public string? MiddleName { get; set; }
+        [StringLength(10)]
         public required string PassportNumSerial { get; set; }
         [DataType(DataType.Date)]
         public required DateTime Birthday { get; set; }
+        [StringLength(20)]
         [Phone] 
         public required string PhoneNumber { get; set; }
         [EmailAddress]
         public required string EmailAddress { get; set; }
+        [StringLength(7)]
         public required string MedCardNum { get; set; }
         [DataType(DataType.Date)]
         public required DateTime MedCardDate { get; set; }
@@ -24,12 +30,11 @@ namespace ClinicBigBoarsWeb.Models
         public required DateTime LatVizitDate { get; set; }
         [DataType(DataType.Date)]
         public required DateTime NextVizitDate { get; set; }
+        [StringLength(16)]
         public required string PolisNum { get; set; }
         [DataType(DataType.Date)]
         public required DateTime PolisDate { get; set; }
         public string? Diagnose { get; set; }
         public string? MedicalHistory { get; set; }
-
-
     }
 }
