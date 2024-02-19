cffd7b0fde0755b48f0c402c763066a3655a8a3d
nigga <nigga@DESKTOP-JPODH2A>
2024-02-19 13:43:50 +0500

    Добавьте файлы проекта.

---
+++ ClinicBigBoarsWeb/Models/Patient.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicBigBoarsWeb.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+        public string? Photo {  get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public string? MiddleName { get; set; }
+        public required string PassportNumSerial { get; set; }
+        [DataType(DataType.Date)]
+        public required DateTime Birthday { get; set; }
+        [Phone] 
+        public required string PhoneNumber { get; set; }
+        [EmailAddress]
+        public required string EmailAddress { get; set; }
+        public required string MedCardNum { get; set; }
+        [DataType(DataType.Date)]
+        public required DateTime MedCardDate { get; set; }
+        [DataType(DataType.Date)]
+        public required DateTime LatVizitDate { get; set; }
+        [DataType(DataType.Date)]
+        public required DateTime NextVizitDate { get; set; }
+        public required string PolisNum { get; set; }
+        [DataType(DataType.Date)]
+        public required DateTime PolisDate { get; set; }
+        public string? Diagnose { get; set; }
+        public string? MedicalHistory { get; set; }
+
+
+    }
+}
